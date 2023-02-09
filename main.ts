7c389cb7a0bf2e6a8d18e502a2fa50b14aafc548
Matthew768AOD <115144150+Matthew768AOD@users.noreply.github.com>
2023-02-09 11:30:31 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.setAccelerometerRange(AcceleratorRange.TwoG)
 basic.forever(function () {
-	
+    
+let r = 0
+let p = 0
+let s = 0
+let z = 0
+let y = 0
+let x = 0
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    z = input.acceleration(Dimension.Z)
+    s = input.acceleration(Dimension.Strength)
+
+input.onButtonPressed(Button.A, function() {
+    
+})
+    
+console.logValue("x", x)
+console.logValue("y", y)
+console.logValue("z", z)
+console.logValue("strength", s)
+basic.pause(100)
 })
